f5885523cb478a52bc41902db218e4a3fde114c9
taboger <gerardoetaboada@gmail.com>
2018-10-15 17:05:42 -0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,13 +94,13 @@ public:
 
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1485561600; // January 28, 2017
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1517356801; // January 31st, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1540065804; // Saturday, 20 October 2018 20:03:24
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1540325004; // JTuesday, 23 October 2018 20:03:24
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1485561600; // January 28, 2017
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1540065804; // Saturday, 20 October 2018 20:03:24
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1540325004; // Tuesday, 23 October 2018 20:03:24
 
         // The best chain should have at least this much work.
          consensus.nMinimumChainWork = uint256S("0x00");
